d0c7297bafaec5c77b198e291ee976f204521b9d
VegetableBird10086 <1391909128@qq.com>
2021-12-08 17:58:59 +0800

    goods controller

---
+++ jibao/src/main/java/com/t09/jibao/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.t09.jibao.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        WebMvcConfigurer.super.addCorsMappings(registry);
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedMethods("POST","GET")
+                .allowedOrigins("*");
+    }
+}
